211bb0783c5bb7fd48027236927bc033e24be68d
corey <coreybatchler7@gmail.com>
2023-09-03 08:48:27 +1000

    fix image loading issue

--- src/areas/home/pages/Home.tsx
+++ src/areas/home/pages/Home.tsx
@@ -1,41 +1,52 @@
 import DayImage from "../../../assets/storyb.jpg";
 import NightImage from "../../../assets/storyb-night.jpg";
 import { useOutletContext } from "react-router-dom";
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
+import { Spinner } from "react-bootstrap";
 
 function Home() {
   useEffect(() => {
     document.title = "Home | batchler.me";
   }, []);
 
-  const { darkMode } = useOutletContext<{
-    darkMode: boolean;
-  }>();
+  const { darkMode } = useOutletContext<{ darkMode: boolean }>();
 
-  const dayByLine =
-    "Brisbane's Story Bridge, Nov. 2019. Taken by me after a few brews at Felon's.";
-  const nightByLine =
-    "Brisbane's Story Bridge, Dec. 2021. Believe it or not, taken by me after a few brews at Felon's.";
+  const images = useMemo(() => [DayImage, NightImage], []);
+  const byLines = [
+    "Brisbane's Story Bridge, Nov. 2019. Taken by me after a few brews at Felon's.",
+    "Brisbane's Story Bridge, Dec. 2021. Believe it or not, taken by me after a few brews at Felon's."
+  ];
 
-  const [srcImage, setSrcImage] = useState(DayImage);
-  const [byLine, setByLine] = useState(dayByLine);
+  const [srcImage, setSrcImage] = useState(images[0]);
+  const [byLine, setByLine] = useState(byLines[0]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [preloadSrc, setPreloadSrc] = useState(images[0]);
 
   useEffect(() => {
-    if (darkMode) {
-      setSrcImage(NightImage);
-      setByLine(nightByLine);
-    } else {
-      setSrcImage(DayImage);
-      setByLine(dayByLine);
-    }
-  }, [darkMode]);
+    setIsLoading(true);
+    setPreloadSrc(images[darkMode ? 1 : 0]);
+  }, [darkMode, images]);
+
+  const handleImageLoad = () => {
+    setSrcImage(preloadSrc);
+    setByLine(byLines[darkMode ? 1 : 0]);
+    setIsLoading(false);
+  };
 
   return (
     <div>
+      {isLoading && <Spinner />}
+      <img
+        src={preloadSrc}
+        alt="Preloading Brisbane's story bridge"
+        style={{ display: 'none' }}
+        onLoad={handleImageLoad}
+      />
       <img
         src={srcImage}
-        alt="Brisbane's story bridge, taken by me in 2019."
+        alt="Brisbane's story bridge, taken by me."
         className="img-fluid"
+        style={{ display: isLoading ? 'none' : 'block' }}
       />
       <p className="figure-text">
         <em>{byLine}</em>
